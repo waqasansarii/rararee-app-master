74a61a330ba3697e399d6aac740d7c1613e7f9ca
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-07-31 01:45:59 +0500

    added slider in landing page

--- src/Components/HomeComp/Reinvite.jsx
+++ src/Components/HomeComp/Reinvite.jsx
@@ -10,10 +10,16 @@ import polygon2 from "../../Assets/Polygon 2@2x.png";
 import cros1 from "../../Assets/Group 26@2x.png";
 import cros2 from "../../Assets/Group 25@3x.png";
 // import Swiper core and required modules
-import SwiperCore, { Autoplay, Pagination, Navigation,Mousewheel } from "swiper/core";
+import SwiperCore, {
+  Autoplay,
+  Pagination,
+  Navigation,
+  Mousewheel,
+} from "swiper/core";
+import ReinviteSlideTwoSection from "./ReinviteSlideTwoSection";
 
 // install Swiper modules
-SwiperCore.use([Autoplay, Pagination, Navigation,Mousewheel]);
+SwiperCore.use([Autoplay, Pagination, Navigation, Mousewheel]);
 
 const Reinvite = () => {
   // const ref1 = useRef(null)
@@ -21,130 +27,92 @@ const Reinvite = () => {
 
   return (
     <div className="reinvite_container">
-      <div className="reinvite_main_div">
-        <div className="reinvite_head_div">
-          <div className="img_detail_div">
-            <img className="polygon1_img" src={polygon1} alt="img" />
-            <div className="reinvite_detail_div">
-              <h2 className="reinventing_head">
-                Reinventing
-                <strong style={{ color: "#19c2a0", marginLeft: "5px" }}>
-                  Citizen
-                </strong>{" "}
-                Engagement
-              </h2>
-              <p className="reinventing_para">
-                We are delivering citizen benefits remotely and at scale to
-                improve quality of life for people in cities and rural areas
-                across Sub-Sahara Africa
-              </p>
-              <Link className="Sign_up_today_link" to="/signup">
-                Sign Up today
-              </Link>
-              <img className="polygon2_img" src={polygon2} alt="img" />
+      <Swiper navigation={true} pagination className='heroSection_slider' >
+        <SwiperSlide>
+          <div className="reinvite_main_div">
+            <div className="reinvite_head_div">
+              <div className="img_detail_div">
+                <img className="polygon1_img" src={polygon1} alt="img" />
+                <div className="reinvite_detail_div">
+                  <h2 className="reinventing_head">
+                    Reinventing
+                    <strong style={{ color: "#19c2a0", marginLeft: "5px" }}>
+                      Citizen
+                    </strong>{" "}
+                    Engagement
+                  </h2>
+                  <p className="reinventing_para">
+                    We are delivering citizen benefits remotely and at scale to
+                    improve quality of life for people in cities and rural areas
+                    across Sub-Sahara Africa
+                  </p>
+                  <Link className="Sign_up_today_link" to="/signup">
+                    Sign Up today
+                  </Link>
+                  <img className="polygon2_img" src={polygon2} alt="img" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className="reinvite_img_div">
-          <div className="cros_img_div">
-            <img className="cros1_home" src={cros1} alt="img" />
-            <img className="cros2_home" src={cros2} alt="img" />
-          </div>
-          <ul className="see_world_ul" id="see_world_ul">
-            <Swiper
-              // pagination
-              spaceBetween={0}
-              slidesPerView={3}
-              centeredSlides
-              initialSlide={1}
-              direction={"vertical"}
-              mousewheel
-              className="myGirls_slider swiper_vertical"
-              style={{paddingLeft:5,paddingRight:5}}
-            >
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 😃 </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li2">You are a citizen of Global Village! 😄</li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 🌎 </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 😄  </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 🌏 </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 😵 </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 😃 </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 🌏 </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 😵 </li>
-              </SwiperSlide>
-              <SwiperSlide className='li_swiper_slides'>
-                <li className="li1">Change how you see the world! 😃 </li>
-              </SwiperSlide>
-            </Swiper>
-          </ul>
-          <Swiper
-            // pagination
-            spaceBetween={50}
-            slidesPerView={1}
-            autoplay={{ disableOnInteraction: false, delay: 2500 }}
-            // style={{ width: "50%" }}
-            className="myGirls_slider"
-          >
-            <SwiperSlide
-              style={{
-                width: "100%",
-                // height: "475px",
-                marginRight: "0px ",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <img className="girl_img" src={girl} alt="img" />
-              {/* <img className="polygon2_img2" src={polygon2} alt="img" /> */}
-              <img className="polygon3_img" src={polygon2} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide
-              style={{
-                width: "100%",
-                // height: "475px",
-                marginRight: "0px ",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              {/* <div className="cros_img_div">
+            <div className="reinvite_img_div">
+              <div className="cros_img_div">
                 <img className="cros1_home" src={cros1} alt="img" />
                 <img className="cros2_home" src={cros2} alt="img" />
               </div>
-              <ul className="see_world_ul">
-                <li className="li1">Change how you see the world! </li>
-                <li className="li2">You are a citizen of Global Village! 😄</li>
-                <li className="li3">Change how you see the world! </li>
-              </ul> */}
+              <ul className="see_world_ul" id="see_world_ul">
+                <Swiper
+                  // pagination
+                  spaceBetween={0}
+                  slidesPerView={3}
+                  centeredSlides
+                  initialSlide={1}
+                  direction={"vertical"}
+                  mousewheel
+                  className="myGirls_slider swiper_vertical"
+                  style={{ paddingLeft: 5, paddingRight: 5 }}
+                >
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 😃 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li2">
+                      You are a citizen of Global Village! 😄
+                    </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 🌎 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 😄 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 🌏 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 😵 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 😃 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 🌏 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 😵 </li>
+                  </SwiperSlide>
+                  <SwiperSlide className="li_swiper_slides">
+                    <li className="li1">Change how you see the world! 😃 </li>
+                  </SwiperSlide>
+                </Swiper>
+              </ul>
               <img className="girl_img" src={girl} alt="img" />
               <img className="polygon3_img" src={polygon2} alt="img" />
-            </SwiperSlide>
-            {/* <img className="girl_img" src={girl} alt="img" /> */}
-            {/* <img className="polygon2_img2" src={polygon2} alt="img" /> */}
-            {/* <img className="polygon3_img" src={polygon2} alt="img" /> */}
-          </Swiper>
-          {/* <img className="polygon2_img2" src={polygon2} alt="img" /> */}
-        </div>
-    
-      </div>
+            </div>
+          </div>
+        </SwiperSlide>
+          <SwiperSlide>
+            <ReinviteSlideTwoSection />
+          </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
---
+++ src/Components/HomeComp/ReinviteSlideTwoSection.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+// assets
+import bg from "../../Assets/slider bg.png";
+import mapImg from "../../Assets/Group 6012.png";
+import location from '../../Assets/Icon material-locationW.png'
+import shadowImg from '../../Assets/slider main shadow.png'
+// import shadowImg from '../../Assets/Group 6048.png'
+
+const ReinviteSlideTwoSection = () => {
+  return (
+    <div className="sliderMap_container">
+      <div className="sliderMap_md">
+        <div className="sliderMap_sd">
+          <div className="sliderMap_left_section">
+            <div>
+              <h5>Discover, View & Feel!</h5>
+              <h1>WASH - AI Driven</h1>
+              <p>
+                United Nations USDG6 (Ensure access to water and sanitation for
+                all)
+              </p>
+              <div className="sliderMap_btns">
+                <button className="cus_grn_btn">Take a tour</button>
+                <button className="sliderMap_cusBtn">
+                  Share with a friend
+                </button>
+              </div>
+            </div>
+            <div className="sliderMap_locations">
+                <h1>Near you!</h1>
+                <div className="sliderMap_loc">
+                    <img src={location} alt="..." />
+                    <p>User current location, City, Country </p>
+                </div>
+                <div className="beforeAfteDiv">
+                    <img src={mapImg} alt="..." />
+                    <img src={mapImg} alt="..." />
+                </div>
+                <p>Source: www.planet.com</p>
+            </div>
+          </div>
+          <div className="sliderMap_right_section">
+            <div className='shadowImg_Container'>
+                <img className='shadow_img' src={shadowImg} alt="..." />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReinviteSlideTwoSection;
